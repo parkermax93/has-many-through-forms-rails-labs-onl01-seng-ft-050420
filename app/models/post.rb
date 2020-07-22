1fdf3098b22fcf6b38844b98ed9a1718238f1834
Parker Williamson <parker.williamson@gmail.com>
2020-07-22 21:33:30 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,20 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: proc {|attr| attr['name'].blank?}
 
+
+  def comments_attributes=(attributes)
+      attributes.values.each do |attr|
+        comment = Comment.find_or_create_by(attr)
+        self.comment_attributes.build(comment: comment)
+      end
+  end
+
+  def uniq_users
+
+      self.comments.uniq { |comment| comment.user.id }
+    
+  end
 
 end
